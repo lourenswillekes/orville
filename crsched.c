430a1c2205c3054be9116083ba5d2112f76ad228
Nicholas Pahl <pahlni@mail.gvsu.edu>
2017-07-27 18:40:20 -0400

    Changes after first week, not working.

--- crsched.c
+++ crsched.c
@@ -13,6 +13,134 @@
 
 #define STACK_SIZE 4096   // Amount of stack space for each thread
 
+void scheduler(void);
+void SVCHandle(void);
+int getPriv(void);
+void handleSVC(int code);
+void threadStarter(void);
+void regStateSave(void);
+void regStateRestore(void);
+
+typedef struct {
+  int active;       // non-zero means thread is allowed to run
+  char *stack;      // pointer to TOP of stack (highest memory location)
+  unsigned savedRegs[10];
+} threadStruct_t;
+
+// thread_t is a pointer to function with no parameters and
+// no return value...i.e., a user-space thread.
+typedef void (*thread_t)(void);
+
+// These are the external user-space threads. In this program, we create
+// the threads statically by placing their function addresses in
+// threadTable[]. A more realistic kernel will allow dynamic creation
+// and termination of threads.
+extern void thread1(void);
+extern void thread2(void);
+
+// The size of used stack + 64 bytes for each thread
+// This aray replaces STACK_SIZE macro
+static int stackSize[] = {
+  STACK_SIZE,
+  STACK_SIZE,
+  STACK_SIZE,
+  STACK_SIZE
+};
+
+static thread_t threadTable[] = {
+  thread1,
+  thread1,
+  thread2,
+  thread2 //all that is needed to add another thread is to add a function to the threadTable
+};
+#define NUM_THREADS (sizeof(threadTable)/sizeof(threadTable[0]))
+
+// These static global variables are used in scheduler(), in
+// the yield() function, and in threadStarter()
+static threadStruct_t threads[NUM_THREADS]; // the thread table
+unsigned currThread;    // The currently active thread
+
+
+
+// This function is implemented in assembly language. It sets up the
+// initial jump-buffer (as would setjmp()) but with our own values
+// for the stack (passed to createThread()) and LR (always set to
+// threadStarter() for each thread).
+extern void createThread(jmp_buf buf, char *stack);
+
+void main(void)
+{
+  unsigned i;
+
+  // Set the clocking to run directly from the crystal.
+  SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+                 SYSCTL_XTAL_8MHZ);
+
+  // Initialize the OLED display and write status.
+  RIT128x96x4Init(1000000);
+  RIT128x96x4StringDraw("Scheduler Demo",       20,  0, 15);
+
+  // Enable the peripherals used by this example.
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+  // Set GPIO A0 and A1 as UART pins.
+  GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+  // Configure the UART for 115,200, 8-N-1 operation.
+  UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
+                      (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
+                       UART_CONFIG_PAR_NONE));
+
+  // Set Systick to be a 1ms timer with interupt
+  NVIC_ST_CTRL_R = 0;
+  NVIC_ST_RELOAD_R = 8000;
+  NVIC_ST_CURRENT_R = 0;
+  NVIC_ST_CTRL_R = 0x7;
+
+  // Create all the threads and allocate a stack for each one
+  for (i=0; i < NUM_THREADS; i++) {
+    // Mark thread as runnable
+    threads[i].active = 1;
+
+    // Allocate stack
+    threads[i].stack = (char *)malloc(stackSize[i]) + stackSize[i];
+    if (threads[i].stack == 0) {
+      iprintf("Out of memory\r\n");
+      exit(1);
+    }
+
+    // sets a threads stack to all 0xff to see how much memory each stacks uses
+    memset(threads[i].stack - stackSize[i], 0xffffffff, stackSize[i]);
+
+    // After createThread() executes, we can execute a longjmp()
+    // to threads[i].state and the thread will begin execution
+    // at threadStarter() with its own stack.
+    createThread(threads[i].state, threads[i].stack);
+  }
+
+  // Enable interrupts
+  IntMasterEnable();
+
+  // Start running coroutines
+  scheduler();
+
+  // If scheduler() returns, all coroutines are inactive and we return
+  // from main() hence exit() should be called implicitly (according to
+  // ANSI C). However, TI's startup_gcc.c code (ResetISR) does not
+  // call exit() so we do it manually.
+  exit(0);
+}
+
+void scheduler(void){
+  regStateSave(threads[currThread].savedRegs, &threads[currThread].savedRegs[32]);
+  if (++currThread == NUM_THREADS) {
+    currThread = 0;
+  }
+  NVIC_ST_CURRENT_R = 0;
+  regStateRestore(threads[currThread].savedRegs, &threads[currThread].savedRegs[32]);
+  return;
+}
 
 void SVCHandle(void){
   asm volatile ("ldr r0, [r13, #24]");
@@ -64,48 +192,6 @@ void handleSVC(int code)
   }
 }
 
-typedef struct {
-  int active;       // non-zero means thread is allowed to run
-  char *stack;      // pointer to TOP of stack (highest memory location)
-  jmp_buf state;    // saved state for longjmp()
-} threadStruct_t;
-
-// thread_t is a pointer to function with no parameters and
-// no return value...i.e., a user-space thread.
-typedef void (*thread_t)(void);
-
-// These are the external user-space threads. In this program, we create
-// the threads statically by placing their function addresses in
-// threadTable[]. A more realistic kernel will allow dynamic creation
-// and termination of threads.
-extern void thread1(void);
-extern void thread2(void);
-
-// The size of used stack + 64 bytes for each thread
-// This aray replaces STACK_SIZE macro
-static int stackSize[] = {
-/*
-  1576,
-  1576,
-  1576
-  */
-  STACK_SIZE, STACK_SIZE, STACK_SIZE
-};
-
-static thread_t threadTable[] = {
-  thread1,
-  thread2,
-  thread2 //all that is needed to add another thread is to add a function to the threadTable
-};
-#define NUM_THREADS (sizeof(threadTable)/sizeof(threadTable[0]))
-
-// These static global variables are used in scheduler(), in
-// the yield() function, and in threadStarter()
-static jmp_buf scheduler_buf;   // saves the state of the scheduler
-static threadStruct_t threads[NUM_THREADS]; // the thread table
-unsigned currThread;    // The currently active thread
-
-
 // This function is called from within user thread context. It executes
 // a jump back to the scheduler. When the scheduler returns here, it acts
 // like a standard function return back to the caller of yield().
@@ -142,13 +228,8 @@ void threadStarter(void)
   yield();
 }
 
-// This function is implemented in assembly language. It sets up the
-// initial jump-buffer (as would setjmp()) but with our own values
-// for the stack (passed to createThread()) and LR (always set to
-// threadStarter() for each thread).
-extern void createThread(jmp_buf buf, char *stack);
-
 // This is the "main loop" of the program.
+/*
 void scheduler(void)
 {
   unsigned i;
@@ -211,67 +292,13 @@ void scheduler(void)
     }
   } while (1);
 }
-
-void main(void)
-{
-  unsigned i;
-
-  // Set the clocking to run directly from the crystal.
-  SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
-                 SYSCTL_XTAL_8MHZ);
-
-  // Initialize the OLED display and write status.
-  RIT128x96x4Init(1000000);
-  RIT128x96x4StringDraw("Scheduler Demo",       20,  0, 15);
-
-  // Enable the peripherals used by this example.
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-
-  // Set GPIO A0 and A1 as UART pins.
-  GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-
-  // Configure the UART for 115,200, 8-N-1 operation.
-  UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
-                      (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
-                       UART_CONFIG_PAR_NONE));
-
-  // Create all the threads and allocate a stack for each one
-  for (i=0; i < NUM_THREADS; i++) {
-    // Mark thread as runnable
-    threads[i].active = 1;
-
-    // Allocate stack
-    threads[i].stack = (char *)malloc(stackSize[i]) + stackSize[i];
-    if (threads[i].stack == 0) {
-      iprintf("Out of memory\r\n");
-      exit(1);
-    }
-
-    // sets a threads stack to all 0xff to see how much memory each stacks uses
-    memset(threads[i].stack - stackSize[i], 0xffffffff, stackSize[i]);
-
-    // After createThread() executes, we can execute a longjmp()
-    // to threads[i].state and the thread will begin execution
-    // at threadStarter() with its own stack.
-    createThread(threads[i].state, threads[i].stack);
-  }
-
-  // Start running coroutines
-  scheduler();
-
-  // If scheduler() returns, all coroutines are inactive and we return
-  // from main() hence exit() should be called implicitly (according to
-  // ANSI C). However, TI's startup_gcc.c code (ResetISR) does not
-  // call exit() so we do it manually.
-  exit(0);
+*/
+void __attribute__((naked)) regStateSave(unsigned *storeReg, unsigned *pspLocation){
+  asm volatile("stmea r0, {r0-r11, r13}");
+  asm volatile("MRS r1, PSP");
 }
 
-/*
- * Compile with:
- * ${CC} -o crsched.elf -I${STELLARISWARE} -L${STELLARISWARE}/driverlib/gcc 
- *     -Tlinkscript.x -Wl,-Map,crsched.map -Wl,--entry,ResetISR 
- *     crsched.c create.S threads.c startup_gcc.c syscalls.c rit128x96x4.c 
- *     -ldriver
- */
-// vim: expandtab ts=2 sw=2 cindent
+void __attribute__((naked)) regStateRestore(unsigned *storeReg, unsigned *pspLocation){
+  asm volatile("MSR PSP, r1");
+  asm volatile("ldmea r0, {r0-r11, r13}");
+}
--- startup_gcc.c
+++ startup_gcc.c
@@ -21,6 +21,7 @@ static void IntDefaultHandler(void);
 //*****************************************************************************
 extern int main(void);
 extern void SVCHandle(void);
+extern void scheduler(void);
 
 //*****************************************************************************
 //
@@ -47,7 +48,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // Debug monitor handler
     0,                                      // Reserved
     IntDefaultHandler,                      // The PendSV handler
-    IntDefaultHandler,                      // The SysTick handler
+    scheduler,                      // The SysTick handler
     IntDefaultHandler,                      // GPIO Port A
     IntDefaultHandler,                      // GPIO Port B
     IntDefaultHandler,                      // GPIO Port C
